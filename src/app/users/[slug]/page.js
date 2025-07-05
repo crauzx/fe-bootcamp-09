7cc3faaa9aca004f8181ab78971f243777e73c6d
anglie-yanto <anglie.yanto@gdn-commerce.com>
2025-07-05 09:50:47 +0700

    remove: passing param

--- src/app/users/[slug]/page.js
+++ src/app/users/[slug]/page.js
@@ -3,5 +3,5 @@ import UserDetailView from "./user-detail-view";
 export default async function UserDetailPage({ params }) {
   const { slug } = params;
 
-  return <UserDetailView user={userSnapshot.data()} />;
+  return <UserDetailView />;
 }
